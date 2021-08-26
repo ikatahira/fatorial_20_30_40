4fdd4925523ff67ac96e7cff3d1757265350b71b
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-08-26 01:42:09 +0000

    fatorial completo

--- Fatorial.java
+++ Fatorial.java
@@ -5,12 +5,13 @@ public class Fatorial{
         //fatorial de 20, 30, 40
 
         //regra n*n-1.....n==1
-        int fatorial=1;
-        for(int i=5; i>=1;i--){
+        long fatorial=1;
+        for(int i=30; i>=1;i--){
             fatorial=fatorial*i;
 
         }
         System.out.println(fatorial);
+
 
     }
 }
